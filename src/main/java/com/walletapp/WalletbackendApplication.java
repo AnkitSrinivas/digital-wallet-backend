415431e6aaa02303ad7b03f0e237cf40b8694634
ankit <ankitsrinivas007@gmail.com>
2026-05-06 20:33:24 +0530

    Intial project setup springboot configuration

---
+++ src/main/java/com/walletapp/WalletbackendApplication.java
@@ -0,0 +1,13 @@
+package com.walletapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletbackendApplication.class, args);
+	}
+
+}
